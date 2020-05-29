cdf48018b671a4e71474e206f4adb9574f3eedd2
John Mack <mackspace@gmail.com>
2020-05-29 16:33:17 -0600

    added netlify functions

--- adventure-montana/functions/api.js
+++ adventure-montana/functions/api.js
@@ -1,8 +1,12 @@
 const { GOOGLE_MAPS_API_KEY } = process.env;
 
-exports.handler = async (event, context, callback) => {
-  callback(null, {
-    statusCode: 200,
-    body: GOOGLE_MAPS_API_KEY
-  })
+exports.handler = async (event, context) => {
+  return GOOGLE_MAPS_API_KEY
 };
+
+// exports.handler = async (event, context, callback) => {
+//   callback(null, {
+//     statusCode: 200,
+//     body: GOOGLE_MAPS_API_KEY
+//   })
+// };
