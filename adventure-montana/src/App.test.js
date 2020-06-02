d8b959f41e2d50e35fa1b2b41e449fc56d7d771e
John Mack <mackspace@gmail.com>
2020-06-02 12:15:33 -0600

    added smoke test for App

--- adventure-montana/src/App.test.js
+++ adventure-montana/src/App.test.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
+import { Provider } from "react-redux";
+import { store } from "./store";
+import AdventureList from './AdventureList';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders App and NavBar', () => {
+  const { getByText } = render(
+    <Provider store={store}>
+      <App />
+    </Provider>);
+    const navbar = getByText("Register");
+    expect (navbar).toBeInTheDocument();
 });
